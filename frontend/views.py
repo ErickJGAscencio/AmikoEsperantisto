ec3aa9276a0aba2a9ec1dc00c82e71f33b1f834f
Erick Jesús Guzmán Ascencio <erickjesus00001@gmail.com>
2024-05-30 22:29:30 -0600

    Diseño vista Libros.html

--- frontend/views.py
+++ frontend/views.py
@@ -4,5 +4,8 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'home.html')
 
+def materiales(request):
+    return render(request, 'materiales.html')
+
 def libros(request):
     return render(request, 'libros.html')
